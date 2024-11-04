e68ed2f7359249ffe7f3d57c291c57563b9e1747
sasoripathos <sasori.pan.jerry@gmail.com>
2024-11-04 22:22:20 +0100

    Should have all 3 cases of member nemesis

--- tests/jepsen/mongodb/src/jepsen/mongodb.clj
+++ tests/jepsen/mongodb/src/jepsen/mongodb.clj
@@ -74,7 +74,7 @@
                          ;:kill      {:targets [:all]}
                          :pause     {:targets [nil :one :primaries :majority :all]}
                          :kill      {:targets [nil :one :primaries :majority :all]}
-                         :member    {:targets [:majority]} ;; {:targets [:primary :minority :majority]}
+                         :member    {:targets [:primary :minority :majority]}
                          :interval  (:nemesis-interval opts)})]
     (merge tests/noop-test
            opts
